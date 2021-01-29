13d52a32ab6e60355433cce38f96638fc099eb62
Yotam Ohad <yotam2206@gmail.com>
2021-01-29 08:54:47 +0200

    added test

--- lab.py
+++ lab.py
@@ -1,11 +1,14 @@
 """
 This script contains tests that we did on the data
 """
+from collections import namedtuple
+
 from torch import optim
 
 from dataloader import get_combined_epoch_png
 from model import create_nn_v1
 import torch
+import numpy as np
 
 import matplotlib.pyplot as plt
 from tqdm import tqdm
@@ -14,6 +17,30 @@ from torchsummary import summary
 
 USE_CUDA = True
 
+class Accuracy(object):
+    def __init__(self):
+        self.correct = 0
+        self.total = 0
+
+    def __str__(self):
+        return f"{self.correct}/{self.total} "\
+               f"[{self.correct/self.total}]"
+
+def _calculate_accuracy(predictions, labels):
+    """
+    Get a prediction tensor BATCHx5 and compares with BATCHx1 class labels
+    """
+    batches, _ = predictions.shape
+    total, correct = 0, 0
+    for i in range(batches):
+        predicted_label = predictions[i].argmax()
+        if predicted_label == labels[i]:
+            correct += 1
+
+        total += 1
+
+    return correct, total
+
 
 def train(model, data_path, epochs=50, batch_size=32):
     criterion = torch.nn.CrossEntropyLoss()
@@ -21,6 +48,7 @@ def train(model, data_path, epochs=50, batch_size=32):
     results = {}
 
     for epoch_index in range(epochs):
+        accuracy = Accuracy()
         data = get_combined_epoch_png(data_path, train=True, batch_size=batch_size)
         for batch_index, (image, label) in enumerate(data):
             if USE_CUDA:
@@ -33,23 +61,43 @@ def train(model, data_path, epochs=50, batch_size=32):
             loss.backward()
             optimizer.step()
 
-        print(f"[{epoch_index}\{epochs}] | Loss: {loss.item():.4f}")
-        results[epoch_index] = {"loss": loss.item()}
+            # save important data for later
+            correct, total = _calculate_accuracy(outputs, label)
+            accuracy.correct += correct
+            accuracy.total += total
+
+        print(f"[{epoch_index}\{epochs}] | Loss: {loss.item():.4f} | "
+              f"accuracy: {accuracy}]")
+        results[epoch_index] = {
+            "loss": loss.item(),
+            "accuracy": accuracy
+        }
 
     return results
 
 
-def test(model, data):
-    pass
+def test(model, data_path, batch_size=32):
+    accuracy = Accuracy()
+    data = get_combined_epoch_png(data_path, train=False, batch_size=batch_size)
+    for batch_index, (image, label) in enumerate(data):
+        if USE_CUDA:
+            image, label = image.cuda(), label.cuda()
+        outputs = model(image)
+
+        correct, total = _calculate_accuracy(outputs, label)
+        accuracy.correct += correct
+        accuracy.total += total
+
+    print(f"Test accuracy is {accuracy}]")
+    return accuracy
 
 
 def simple_run(dataset_path):
     model = create_nn_v1()
 
     model.train()
-    results = train(model, "D:/Clouds/YotamDataSet/96x96_32x32", epochs=100)
+    results = train(model, "D:/Clouds/YotamDataSet/96x96_32x32", epochs=50)
     print(results)
 
     model.eval()
-    test_set = get_combined_epoch_png(dataset_path, train=False)
-    test(model, test_set)
+    results = test(model, "D:/Clouds/YotamDataSet/96x96_32x32")
